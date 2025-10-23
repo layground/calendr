884ba7f06174bfead539faec86860cd12cd48fd5
Agil Setiawan <agilsetiawan772@gmail.com>
2025-10-23 21:27:37 +0700

    Update MonthView and default card styling

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -6,10 +6,10 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
 ));
 Card.displayName = "Card";
 
-const CardHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn("flex flex-col space-y-1.5 p-4 sm:p-6", className)} {...props} />;
+const CardHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn("flex flex-col space-y-1.5 p-2 sm:p-4", className)} {...props} />;
 const CardTitle = ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => <h3 className={cn("text-lg font-semibold leading-none tracking-tight", className)} {...props} />;
 const CardDescription = ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => <p className={cn("text-sm text-slate-500 dark:text-slate-400", className)} {...props} />;
-const CardContent = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn("p-4 sm:p-6 pt-0", className)} {...props} />;
-const CardFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn("flex items-center p-4 sm:p-6 pt-0", className)} {...props} />;
+const CardContent = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn(" p-2 sm:p-4 pt-0", className)} {...props} />;
+const CardFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn("flex items-center  p-2 sm:p-4 pt-0", className)} {...props} />;
 
 export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter };
